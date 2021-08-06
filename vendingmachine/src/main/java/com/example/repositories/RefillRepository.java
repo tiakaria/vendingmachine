13780f893e9a40143ddbc77d391de234aeccdf01
VIET LE <vietle211@gmail.com>
2021-08-05 22:12:36 -0400

    Fix JPA repository, add services

---
+++ vendingmachine/src/main/java/com/example/repositories/RefillRepository.java
@@ -0,0 +1,13 @@
+package com.example.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+import com.example.entities.Refill;
+
+@CrossOrigin()
+@Repository
+public interface RefillRepository extends JpaRepository<Refill, Integer> {
+
+}
